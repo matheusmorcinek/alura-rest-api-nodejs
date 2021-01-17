a0d01eb4fc3b87f0d07aaba84307e69f18b82de8
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-01-17 14:58:41 -0300

    criando arquivo para inicializar tabelas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
-
+const tabelas = require('./infraestrutura/tabelas');
 
 conexao.connect((erro) => {
     if (erro) {
+        
         console.log('deu erro');
     } else {
+        
         console.log('Mysql conectado com sucesso!');
 
+        tabelas.init(conexao);
 
         const app = customExpress();
 
