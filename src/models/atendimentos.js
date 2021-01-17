1c472a808ba4862fac11dc5f8b6da2e8dcb6fef4
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-01-17 15:49:32 -0300

    manipulando datas com a lib moment

--- src/models/atendimentos.js
+++ src/models/atendimentos.js
@@ -1,12 +1,19 @@
 const conexao = require('../infraestrutura/conexao');
+const moment = require('moment');
 
 class Atendimento {
 
     adicona(atendimento) {
 
+        const dataCriacao = new Date();
+        // const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const atendimentoDatadao = {...atendimento, dataCriacao, data};
+
         const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatadao, (erro, resultados) => {
 
             if (erro) {
 
